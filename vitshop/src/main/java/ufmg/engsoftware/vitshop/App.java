1d91c0d0453352a1f8d4c4bf37208d863d8e0fa9
Vitória Mirella <vitorian@ciandt.com>
2022-01-08 19:24:32 -0300

    Estrutura inicial do projeto

---
+++ vitshop/src/main/java/ufmg/engsoftware/vitshop/App.java
@@ -0,0 +1,13 @@
+package ufmg.engsoftware.vitshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
